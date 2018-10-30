07f68ada46b436b97b074956a116d54585ef3a67
Jason Mitchell <jasonmitchell@live.co.uk>
2018-10-30 21:09:30 +0000

    Adding comments to Link

--- Hallo/Link.cs
+++ Hallo/Link.cs
@@ -3,16 +3,38 @@ using System.Text.RegularExpressions;
 
 namespace Hallo
 {
+    /// <summary>
+    /// A struct for defining a hyperlink from the containing resource to a URI
+    /// </summary>
     public readonly struct Link
     {
+        /// <summary>
+        /// The "self" relation
+        /// </summary>
         public const string Self = "self";
         
         private static readonly Regex IsTemplatedRegex = new Regex(@"{.+}", RegexOptions.Compiled);
         
+        /// <summary>
+        /// The relation of the hyperlink to the containing resource
+        /// </summary>
         public string Rel { get; }
+        
+        /// <summary>
+        /// The URI or URI template of the hyperlink
+        /// </summary>
         public string Href { get; }
+        
+        /// <summary>
+        /// Indicates if the hyperlink contains a URI template
+        /// </summary>
         public bool Templated => IsTemplatedRegex.IsMatch(Href);
 
+        /// <summary>
+        /// Initializes a new instance of <see cref="Link"/>
+        /// </summary>
+        /// <param name="rel">The relation of the hyperlink</param>
+        /// <param name="href">The URI or URI template of the hyperlink</param>
         public Link(string rel, string href)
         {
             if (string.IsNullOrWhiteSpace(rel))
