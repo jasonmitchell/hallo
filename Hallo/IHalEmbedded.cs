298c6d746752ac59792b0b564fa46831a4f6bf91
Jason Mitchell <jasonmitchell@live.co.uk>
2018-10-30 22:22:08 +0000

    Add comments for generator interfaces

--- Hallo/IHalEmbedded.cs
+++ Hallo/IHalEmbedded.cs
@@ -2,13 +2,35 @@ using System.Threading.Tasks;
 
 namespace Hallo
 {
-    public interface IHalEmbedded<in T>
+    /// <summary>
+    /// Provides generation of embedded resources for HAL documents
+    /// </summary>
+    /// <typeparam name="TResource">The type of resource the generator applies to</typeparam>
+    public interface IHalEmbedded<in TResource>
     {
-        object EmbeddedFor(T resource);
+        /// <summary>
+        /// Generates embedded resources for the HAL document
+        /// </summary>
+        /// <param name="resource">The instance of the resource to generate embedded resources for</param>
+        /// <returns>The embedded resources</returns>
+        object EmbeddedFor(TResource resource);
     }
 
-    public interface IHalEmbeddedAsync<in T>
+    /// <summary>
+    /// Provides asynchronous generation of embedded resources for HAL documents
+    /// </summary>
+    /// <remarks>
+    /// Asynchronous operations take precedence over synchronous operations
+    /// when both interfaces are implemented.
+    /// </remarks>
+    /// <typeparam name="TResource">The type of resource the generator applies to</typeparam>
+    public interface IHalEmbeddedAsync<in TResource>
     {
-        Task<object> EmbeddedForAsync(T resource);
+        /// <summary>
+        /// Generates embedded resources for the HAL document
+        /// </summary>
+        /// <param name="resource">The instance of the resource to generate embedded resources for</param>
+        /// <returns>The (awaitable) embedded resources</returns>
+        Task<object> EmbeddedForAsync(TResource resource);
     }
 }
--- Hallo/IHalState.cs
+++ Hallo/IHalState.cs
@@ -2,13 +2,35 @@ using System.Threading.Tasks;
 
 namespace Hallo
 {
-    public interface IHalState<in T>
+    /// <summary>
+    /// Provides generation of state for HAL documents
+    /// </summary>
+    /// <typeparam name="TResource">The type of resource the generator applies to</typeparam>
+    public interface IHalState<in TResource>
     {
-        object StateFor(T resource);
+        /// <summary>
+        /// Generates state for the HAL document
+        /// </summary>
+        /// <param name="resource">The instance of the resource to derive state from</param>
+        /// <returns>The derived state</returns>
+        object StateFor(TResource resource);
     }
     
-    public interface IHalStateAsync<in T>
+    /// <summary>
+    /// Provides asynchronous generation of state for HAL documents
+    /// </summary>
+    /// <remarks>
+    /// Asynchronous operations take precedence over synchronous operations
+    /// when both interfaces are implemented.
+    /// </remarks>
+    /// <typeparam name="TResource">The type of resource the generator applies to</typeparam>
+    public interface IHalStateAsync<in TResource>
     {
-        Task<object> StateForAsync(T resource);
+        /// <summary>
+        /// Generates state for the HAL document
+        /// </summary>
+        /// <param name="resource">The instance of the resource to derive state from</param>
+        /// <returns>The (awaitable) derived state</returns>
+        Task<object> StateForAsync(TResource resource);
     }
 }
