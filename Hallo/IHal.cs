0b1f853c5f2de782118a017ea2d08aa43119466c
Jason Mitchell <jasonmitchell@live.co.uk>
2018-10-30 21:20:10 +0000

    Adding comments to Hal

--- Hallo/IHal.cs
+++ Hallo/IHal.cs
@@ -4,6 +4,11 @@ namespace Hallo
 {
     public interface IHal
     {
+        /// <summary>
+        /// Generates a <see cref="HalRepresentation"/> of the provided resource
+        /// </summary>
+        /// <param name="resource">The object to generate a <see cref="HalRepresentation"/> of</param>
+        /// <returns><see cref="HalRepresentation"/></returns>
         Task<HalRepresentation> RepresentationOfAsync(object resource);
     }
 }
